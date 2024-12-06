bc87d423ab922164b196255d9436cae6f4337066
Valentin Gheoace <valentin_gheoace@yahoo.com>
2024-12-06 04:20:37 +0200

    tema_2 Added inc&src folders

--- src/CowFarm.cpp
+++ src/CowFarm.cpp
@@ -1,1 +1,33 @@
 #include "../inc/CowFarm.hpp"
+#include <string>
+
+using namespace std;
+
+// Copy constructor
+CowFarm::CowFarm(const CowFarm& cowFarm)
+    : Farm(cowFarm), cowNumbers(cowFarm.cowNumbers), dailyMilkProduction(cowFarm.dailyMilkProduction)
+{
+    cout << "[#COW_FARM_INFO]: Copierea fermei de vaci cu numele " << cowFarm.Name
+         << " si adresa " << cowFarm.Address << "." << endl
+         << "[#COW_FARM_INFO]: Nr. vaci = " << cowNumbers
+         << " vaci si productia zilnica de lapte = " << dailyMilkProduction << " litri." << endl;
+}
+
+// Move constructor
+CowFarm::CowFarm(CowFarm&& cowFarm) noexcept
+    : Farm(move(cowFarm)), cowNumbers(cowFarm.cowNumbers), dailyMilkProduction(cowFarm.dailyMilkProduction)
+{
+    cout << "[#COW_FARM_INFO]: Mutarea fermei de vaci cu numele " << cowFarm.Name
+         << " si adresa " << cowFarm.Address << "." << endl;
+
+    // Resetăm starea obiectului sursă
+    cowFarm.cowNumbers = 0;
+    cowFarm.dailyMilkProduction = 0.0;
+}
+
+// Destructor
+CowFarm::~CowFarm()
+{
+    cout << "[#COW_FARM_INFO]: Ferma de vaci cu numele " << Name
+         << " si adresa " << Address << " a fost distrusa." << endl;
+}
--- src/Farm.cpp
+++ src/Farm.cpp
@@ -1,1 +1,26 @@
 #include "../inc/Farm.hpp"
+#include <string>
+
+using namespace std;
+
+// Copy-constructor
+Farm::Farm(const Farm& farm) : Name(farm.Name), Address(farm.Address)
+{
+    cout << "[#FARM_INFO]: Copierea fermei cu numele " << farm.Name << " si adresa " << farm.Address << "." << endl;
+}
+
+// Move-constructor
+Farm::Farm(Farm&& farm) noexcept : Name(move(farm.Name)), Address(move(farm.Address))
+{
+    cout << "[#FARM_INFO]: Mutarea fermei cu numele " << farm.Name << " si adresa " << farm.Address << "." << endl;
+
+    // Resetăm starea obiectului sursă
+    farm.Name = "";
+    farm.Address = "";
+}
+
+// Destructor
+Farm::~Farm()
+{
+    cout << "[#FARM_INFO]: Ferma cu numele " << Name << " si adresa " << Address << " a fost distrusa." << endl;
+}
