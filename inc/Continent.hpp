c270ef4004607efc0db2373f56760932ff1784c0
Valentin Gheoace <valentin_gheoace@yahoo.com>
2024-12-13 08:03:55 +0200

    tema_3: Added headers files

---
+++ inc/Continent.hpp
@@ -0,0 +1,24 @@
+#ifndef CONTINENT_HPP
+#define CONTINENT_HPP
+
+#include "country.hpp"
+#include <string>
+#include <vector>
+#include <mutex>
+
+using namespace std;
+
+class Continent {
+private:
+    string name;
+    vector<shared_ptr<Country>> countries;
+    mutable mutex mtx;
+
+public:
+    Continent(const string& continentName);
+    void AddCountry(shared_ptr<Country> country);
+    void ShowCountries() const;
+    string GetName() const;
+};
+
+#endif
---
+++ inc/Country.hpp
@@ -0,0 +1,20 @@
+#ifndef COUNTRY_HPP
+#define COUNTRY_HPP
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+using namespace std;
+
+class Country {
+private:
+    string name;
+
+public:
+    Country(const string& countryName);
+    void ShowCountryInfo() const;
+    string GetCountryName() const;
+};
+
+#endif // COUNTRY_HPP
